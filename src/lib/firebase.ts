ae1b1ea0cdf63ad71d4f0e4f76581c10c19d4a06
Garrett Bland <gmorganbland@gmail.com>
2025-01-05 22:51:11 -0600

    feat: setup firebase auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 // Your Firebase configuration object
 const firebaseConfig = {
@@ -17,3 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 export const db = getFirestore(app);
+
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,26 +1,46 @@
 <script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 	import { ADMIN_NAV_LINKS } from '$lib/constants';
+	import { authState } from '$lib/auth';
+	import LogoutButton from '$lib/components/LogoutButton.svelte';
+
 	let { children } = $props();
 
-	// TO DO: check auth and redirect if not logged in
+	$effect(() => {
+		if (!$authState.loading && !$authState.user) {
+			console.log('user is not logged in');
+			goto('/login');
+		}
+	});
 </script>
 
-<div class="grid grid-cols-12 gap-6">
-	<div class="col-span-3">
-		<div class="max-w-32 border-e-2 border-gray-200 dark:border-neutral-700">
-			<nav class="-me-0.5 flex flex-col space-y-3">
-				{#each ADMIN_NAV_LINKS as link}
-					<a
-						class="inline-flex items-center gap-2 whitespace-nowrap border-e-2 border-transparent py-1 pe-4 text-sm text-gray-500 no-underline hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500"
-						href={link.href}
-					>
-						{link.name}
-					</a>
-				{/each}
-			</nav>
+{#if $authState.loading || !$authState.user}
+	<p>Loading...</p>
+{:else}
+	<div class="grid grid-cols-12 gap-6">
+		<div class="col-span-3">
+			<div class="max-w-32 border-e-2 border-gray-200 dark:border-neutral-700">
+				<nav class="-me-0.5 flex flex-col space-y-3">
+					{#each ADMIN_NAV_LINKS as link}
+						<a
+							class="inline-flex items-center gap-2 whitespace-nowrap border-e-2 border-transparent py-1 pe-4 text-sm text-gray-500 no-underline hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500"
+							href={link.href}
+						>
+							{link.name}
+						</a>
+					{/each}
+				</nav>
+				{#if $authState.user}
+					<p>Welcome, {$authState.user.email}</p>
+					<LogoutButton />
+				{:else}
+					<a href="/login">Login</a>
+				{/if}
+			</div>
+		</div>
+		<div class="col-span-9">
+			{@render children()}
 		</div>
 	</div>
-	<div class="col-span-9">
-		{@render children()}
-	</div>
-</div>
+{/if}
