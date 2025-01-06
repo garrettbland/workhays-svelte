e84fdcd8845fc68a0c758b60e663ba02777e046d
Garrett Bland <gmorganbland@gmail.com>
2025-01-06 14:16:34 -0600

    feat: pageData fix

--- src/routes/jobs/[jobId]/+page.svelte
+++ src/routes/jobs/[jobId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	let { job }: { job: PageData } = $props();
+	let { data }: { data: PageData } = $props();
+	const job = $derived(data);
 </script>
 
 <h1>{job.title}</h1>
