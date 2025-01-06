877c09940a8804739a342ae265d24bbb69353a76
Garrett Bland <gmorganbland@gmail.com>
2025-01-05 21:03:30 -0600

    feat: jobs fetching from firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your Firebase configuration object
+const firebaseConfig = {
+	apiKey: 'AIzaSyDcBWMmheioVRfD6qUlz9FL26HHLsSNDfI',
+	authDomain: 'workhays-firebase.firebaseapp.com',
+	databaseURL: 'https://workhays-firebase.firebaseio.com',
+	projectId: 'workhays-firebase',
+	storageBucket: 'workhays-firebase.appspot.com',
+	messagingSenderId: '154850091334',
+	appId: '1:154850091334:web:d04f857e51be6be36fc547'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore(app);
